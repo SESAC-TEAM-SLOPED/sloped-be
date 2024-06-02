fe28a43c8205975e42579c50104661fb94f05c46
sylee <sylee6529@gmail.com>
2024-06-02 21:42:17 +0900

    Feat: 프로젝트 세팅

---
+++ src/main/java/org/sesac/slopedbe/SlopedBeApplication.java
@@ -0,0 +1,13 @@
+package org.sesac.slopedbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlopedBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlopedBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sesac/slopedbe/SlopedBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sesac.slopedbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlopedBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
