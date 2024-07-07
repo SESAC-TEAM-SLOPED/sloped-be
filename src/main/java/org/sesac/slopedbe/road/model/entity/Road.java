c055ff7dc4b52105704725042234caa3209f3245
bsjin1122 <kitty7579@hanmail.net>
2024-07-07 22:08:06 +0900

    feat: Road 엔터티에 Point타입의 컬럼으로 추가 (#80)

--- src/main/java/org/sesac/slopedbe/road/model/entity/Road.java
+++ src/main/java/org/sesac/slopedbe/road/model/entity/Road.java
@@ -2,6 +2,9 @@ package org.sesac.slopedbe.road.model.entity;
 
 import java.math.BigDecimal;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
 import org.sesac.slopedbe.common.entity.BaseTimeEntity;
 
 import jakarta.persistence.Column;
@@ -10,28 +13,51 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Table(name = "road")
 @Entity
+@Builder
 public class Road extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column()
     private Long id;
 
+    @Transient
     @Column(nullable = false)
     private BigDecimal latitude;
 
+    @Transient
     @Column(nullable = false)
     private BigDecimal longitude;
+
+    @Column(columnDefinition = "geometry(Point, 4326)", nullable = false)
+    private Point point;
 
     private String content;
 
     @Column(name = "address", nullable = false)
     private String address;
+    @Builder
+    public static Road createRoad(BigDecimal latitude, BigDecimal longitude, String content, String address) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+        Point point = geometryFactory.createPoint(new Coordinate(longitude.doubleValue(), latitude.doubleValue()));
+        return Road.builder()
+            .latitude(latitude)
+            .longitude(longitude)
+            .point(point)
+            .content(content)
+            .address(address)
+            .build();
+    }
+
 }
