86f23adc21326186cb15e79d60c3a4fbc259b96a
younngg <8533283@naver.com>
2024-07-08 13:31:21 +0900

    Test: Bookmark Service 테스트 작성(#79)

---
+++ src/test/java/org/sesac/slopedbe/bookmark/service/BookmarkServiceTest.java
@@ -0,0 +1,94 @@
+package org.sesac.slopedbe.bookmark.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.sesac.slopedbe.bookmark.model.entity.Bookmark;
+import org.sesac.slopedbe.bookmark.model.entity.BookmarkId;
+import org.sesac.slopedbe.bookmark.repository.BookmarkRepository;
+import org.sesac.slopedbe.facility.model.entity.Facility;
+import org.sesac.slopedbe.facility.model.type.FacilityType;
+import org.sesac.slopedbe.facility.repository.FacilityRepository;
+import org.sesac.slopedbe.member.model.entity.Member;
+import org.sesac.slopedbe.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Import({BookmarkServiceImpl.class})
+public class BookmarkServiceTest {
+
+	@Autowired
+	private BookmarkService bookmarkService;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private FacilityRepository facilityRepository;
+	@Autowired
+	private BookmarkRepository bookmarkRepository;
+
+	@BeforeEach
+	public void setUp() {
+		System.out.println("setup........");
+		Member member = new Member("example@email.com", LocalDateTime.now(), LocalDateTime.now());
+		memberRepository.save(member);
+
+		Facility facility = new Facility("", true, true, "", 3D, 3D, "", "", FacilityType.CAFE, "", true);
+		facilityRepository.save(facility);
+
+	}
+
+	@AfterEach
+	public void afterEach() {
+		System.out.println("deleteAll........");
+		bookmarkRepository.deleteAll();
+	}
+
+	@Test
+	public void addBookmarkTest() {
+		System.out.println("addBookmarkTest........");
+		Member member = memberRepository.findByEmail("example@email.com").get();
+		Facility facility = facilityRepository.findById(1L).get();
+
+		bookmarkService.addBookmark("example@email.com", 1L);
+
+		BookmarkId bookmarkId = new BookmarkId(facility, member);
+
+		assertTrue(bookmarkRepository.findById(bookmarkId).isPresent());
+	}
+
+	@Test
+	public void removeBookmarkTest() {
+		System.out.println("removeBookmarkTest........");
+		Member member = memberRepository.findByEmail("example@email.com").get();
+		Facility facility = facilityRepository.findById(1L).get();
+
+
+
+		bookmarkService.removeBookmark("example@email.com", 1L);
+
+		BookmarkId bookmarkId = new BookmarkId(facility, member);
+		assertTrue(bookmarkRepository.findById(bookmarkId).isEmpty());
+	}
+
+	@Test
+	public void getBookmarksByMemberEmailTest() {
+		System.out.println("getBookmarksByMemberEmailTest........");
+		bookmarkService.addBookmark("example@email.com", 1L);
+		List<Bookmark> bookmarks = bookmarkService.getBookmarksByMemberEmail("example@email.com");
+
+		assertThat(bookmarks.size()).isEqualTo(1);
+	}
+}
