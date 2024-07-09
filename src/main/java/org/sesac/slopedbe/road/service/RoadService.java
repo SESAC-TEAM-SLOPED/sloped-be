ac5e3b99c23f2e0a5b40082aad737e07c812a643
bsjin1122 <kitty7579@hanmail.net>
2024-07-09 17:16:40 +0900

    chore: road 관련 코드 삭제(#23)

--- src/main/java/org/sesac/slopedbe/road/service/RoadService.java
+++ src/main/java/org/sesac/slopedbe/road/service/RoadService.java
@@ -1,11 +1,5 @@
 package org.sesac.slopedbe.road.service;
 
-import java.math.BigDecimal;
-
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
-import org.sesac.slopedbe.road.model.entity.Road;
 import org.sesac.slopedbe.road.repository.RoadRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +10,5 @@ import lombok.RequiredArgsConstructor;
 public class RoadService {
 
     private final RoadRepository roadRepository;
-
-    public Road createLocation() {
-        GeometryFactory geometryFactory = new GeometryFactory();
-        BigDecimal latitude = new BigDecimal("37.7749");
-        BigDecimal longitude = new BigDecimal("-122.4194");
-        String content = "This is a road.";
-        String address = "123 Road St.";
-        Point point = geometryFactory.createPoint(new Coordinate(longitude.doubleValue(), latitude.doubleValue()));
-
-        Road road = Road.createRoad(latitude, longitude, content, address);
-        return roadRepository.save(road);
-    }
 
 }
