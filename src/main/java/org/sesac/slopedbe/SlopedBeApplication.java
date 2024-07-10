70089e58713c9a225b7505af47183a360b4cc971
bsjin1122 <kitty7579@hanmail.net>
2024-07-10 22:21:08 +0900

    Fix: @EnableJpaAuditing 삭제(#23)

--- src/main/java/org/sesac/slopedbe/SlopedBeApplication.java
+++ src/main/java/org/sesac/slopedbe/SlopedBeApplication.java
@@ -2,10 +2,8 @@ package org.sesac.slopedbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SlopedBeApplication {
 
     public static void main(String[] args) {
