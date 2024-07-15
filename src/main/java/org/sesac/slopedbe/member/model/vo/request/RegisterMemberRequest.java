3c7e594079f5c7995c3ff1726ca2fb73dcfbf781
sylee <sylee6529@gmail.com>
2024-07-15 22:55:59 +0900

    Fix: Valid 관련 Exception 메시지 처리 수정 (#21)

--- src/main/java/org/sesac/slopedbe/member/model/vo/request/RegisterMemberRequest.java
+++ src/main/java/org/sesac/slopedbe/member/model/vo/request/RegisterMemberRequest.java
@@ -5,15 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RegisterMemberRequest (
-	@NotBlank(message = "아이디를 입력해주세요.")
+	@NotBlank(message = "아이디를 입력해주세요")
 	String id,
-	@NotBlank(message = "비밀번호를 입력해주세요.")
+	@NotBlank(message = "비밀번호를 입력해주세요")
 	String password,
-	@NotBlank(message = "이메일을 입력해주세요.")
-	@Email(message = "이메일 형식이 아닙니다.")
+	@NotBlank(message = "이메일을 입력해주세요")
+	@Email(message = "이메일 형식이 아닙니다")
 	String email,
-	@NotBlank(message = "닉네임을 입력해주세요.")
+	@NotBlank(message = "닉네임을 입력해주세요")
 	String nickname,
-	@NotNull(message = "장애 여부를 입력해주세요.")
+	@NotNull(message = "장애 여부를 입력해주세요")
 	Boolean isDisabled
 ){}
