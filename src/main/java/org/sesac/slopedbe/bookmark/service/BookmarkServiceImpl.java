dedd91f6cf8ebbf6a6386c0fe8ef1c6f4699f5f3
Jiyeong Jeong <8533283@naver.com>
2024-07-20 13:06:07 +0900

    Feat: 즐겨찾기 중복 및 회원, 시설 부재시 예외처리

--- src/main/java/org/sesac/slopedbe/bookmark/service/BookmarkServiceImpl.java
+++ src/main/java/org/sesac/slopedbe/bookmark/service/BookmarkServiceImpl.java
@@ -2,14 +2,21 @@ package org.sesac.slopedbe.bookmark.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.sesac.slopedbe.bookmark.exception.BookmarkErrorCode;
+import org.sesac.slopedbe.bookmark.exception.BookmarkException;
 import org.sesac.slopedbe.bookmark.model.dto.BookmarkRequestDTO;
 import org.sesac.slopedbe.bookmark.model.dto.BookmarkResponseDTO;
 import org.sesac.slopedbe.bookmark.model.entity.Bookmark;
 import org.sesac.slopedbe.bookmark.model.entity.BookmarkId;
 import org.sesac.slopedbe.bookmark.repository.BookmarkRepository;
+import org.sesac.slopedbe.common.exception.BaseException;
+import org.sesac.slopedbe.common.exception.GlobalErrorCode;
 import org.sesac.slopedbe.facility.model.entity.Facility;
 import org.sesac.slopedbe.facility.repository.FacilityRepository;
+import org.sesac.slopedbe.member.exception.MemberErrorCode;
+import org.sesac.slopedbe.member.exception.MemberException;
 import org.sesac.slopedbe.member.model.entity.Member;
 import org.sesac.slopedbe.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
@@ -26,22 +33,30 @@ public class BookmarkServiceImpl implements BookmarkService {
 
     @Override
     public void addBookmark(String email, BookmarkRequestDTO bookmarkRequestDTO) {
-        System.out.println(bookmarkRequestDTO.getFacilityId());
-        Member member = memberRepository.findByEmail(email).orElse(null);
-        Facility facility = facilityRepository.findById(bookmarkRequestDTO.getFacilityId()).orElse(null);
+        Member member = memberRepository.findByEmail(email).orElseThrow(()->
+            new MemberException(MemberErrorCode.MEMBER_ID_NOT_FOUND));
+        Facility facility = facilityRepository.findById(bookmarkRequestDTO.getFacilityId()).orElseThrow(()->new BaseException(
+            GlobalErrorCode.BAD_REQUEST));
 
-        // 중복 제거
+        BookmarkId bookmarkId = new BookmarkId(facility, member);
 
-        System.out.println("facilityId............" + facility);
-        Bookmark bookmark = Bookmark.create(facility, member);
+        Optional<Bookmark> bookmark = bookmarkRepository.findById(bookmarkId);
 
-        bookmarkRepository.save(bookmark);
+        if(bookmark.isPresent()) {
+            throw new BookmarkException(BookmarkErrorCode.BOOKMARK_ALREADY_EXISTS);
+        }
+
+        Bookmark newBookmark = Bookmark.create(facility, member);
+
+        bookmarkRepository.save(newBookmark);
     }
 
     @Override
     public void removeBookmark(String email, BookmarkRequestDTO bookmarkRequestDTO) {
-        Member member = memberRepository.findByEmail(email).orElse(null);
-        Facility facility = facilityRepository.findById(bookmarkRequestDTO.getFacilityId()).orElse(null);
+        Member member = memberRepository.findByEmail(email).orElseThrow(()->
+            new MemberException(MemberErrorCode.MEMBER_ID_NOT_FOUND));
+        Facility facility = facilityRepository.findById(bookmarkRequestDTO.getFacilityId()).orElseThrow(()->new BaseException(
+            GlobalErrorCode.BAD_REQUEST));
 
         BookmarkId bookmarkId = new BookmarkId(facility, member);
         System.out.println(bookmarkId);
--- src/test/java/org/sesac/slopedbe/bookmark/service/BookmarkServiceTest.java
+++ src/test/java/org/sesac/slopedbe/bookmark/service/BookmarkServiceTest.java
@@ -1,22 +1,8 @@
 package org.sesac.slopedbe.bookmark.service;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.sesac.slopedbe.bookmark.model.entity.Bookmark;
-import org.sesac.slopedbe.bookmark.model.entity.BookmarkId;
 import org.sesac.slopedbe.bookmark.repository.BookmarkRepository;
-import org.sesac.slopedbe.facility.model.entity.Facility;
-import org.sesac.slopedbe.facility.model.type.FacilityType;
 import org.sesac.slopedbe.facility.repository.FacilityRepository;
-import org.sesac.slopedbe.member.model.entity.Member;
 import org.sesac.slopedbe.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
