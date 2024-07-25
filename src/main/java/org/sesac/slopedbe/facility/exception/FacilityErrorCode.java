90bf27af3e73b4be1dc1ef982fe26baf590dc82d
sylee <sylee6529@gmail.com>
2024-07-25 18:00:23 +0900

    Fix: Facility 주석 수정 (#34)

--- src/main/java/org/sesac/slopedbe/facility/exception/FacilityErrorCode.java
+++ src/main/java/org/sesac/slopedbe/facility/exception/FacilityErrorCode.java
@@ -9,9 +9,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum FacilityErrorCode implements ErrorCode {
-	FACILITY_NOT_FOUND(HttpStatus.NOT_FOUND, "Facility not found"),
-	FACILITY_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "Facility type not found"),
-	FIND_FACILITY_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "Limit exceeded");
+	FACILITY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 시설입니다."),
+	FACILITY_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "시설 타입은 RESTAURANT, CAFE, PUBLIC_SPACE, ETC 중 하나여야 합니다."),
+	FIND_FACILITY_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "시설은 100개 이하로 조회 가능합니다."),;
 
 	private final HttpStatus status;
 	private final String message;
