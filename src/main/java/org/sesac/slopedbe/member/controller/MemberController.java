4cf31b2fa1684f55ced7585c596f4fd70d58cbf9
ChangHoSe <sas1397@naver.com>
2024-08-03 16:37:03 +0900

    Feat : Security Filter문제 관련 Controller 경로 수정 (#103)

--- src/main/java/org/sesac/slopedbe/member/controller/MemberController.java
+++ src/main/java/org/sesac/slopedbe/member/controller/MemberController.java
@@ -5,22 +5,18 @@ import java.util.Map;
 
 import org.sesac.slopedbe.member.exception.MemberException;
 import org.sesac.slopedbe.member.model.DTO.request.MemberRequest;
-import org.sesac.slopedbe.member.model.DTO.request.MemberSimpleRequest;
 import org.sesac.slopedbe.member.model.entity.Member;
 import org.sesac.slopedbe.member.model.type.MemberOauthType;
 import org.sesac.slopedbe.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,90 +27,6 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberController {
 
     private final MemberService memberService;
-
-    @Operation(summary = "중복 아이디 검사", description = "회원 가입용, 동일 아이디가 존재하는 지 검사한다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "MemberId가 중복되지 않습니다."),
-        @ApiResponse(responseCode = "409", description = "MemberId가 중복됩니다.")
-    })
-    @PostMapping("/duplicate-check/id")
-    public ResponseEntity<Map<String, String>> checkDuplicateId(@RequestBody MemberSimpleRequest memberRequest) {
-        Map<String, String> response = new HashMap<>();
-
-        try {
-            String memberId = memberRequest.memberId();
-            memberService.checkDuplicateId(memberId);
-            response.put("message", "MemberId가 중복되지 않습니다.");
-            return ResponseEntity.ok(response);
-        } catch (MemberException e) {
-            response.put("error", e.getMessage());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
-        }
-    }
-
-    @Operation(summary = "중복 아이디 검사", description = "아이디 찾기용, 중복 아이디가 존재하는 지 검사한다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "MemberId가 존재합니다."),
-        @ApiResponse(responseCode = "404", description = "MemberId가 존재하지 않습니다.")
-    })
-    @PostMapping("/duplicate-check/find-id")
-    public ResponseEntity<Map<String, String>> checkExistedId(@RequestBody MemberSimpleRequest memberRequest) {
-        Map<String, String> response = new HashMap<>();
-
-        try {
-            String memberId = memberRequest.memberId();
-            memberService.checkExistedId(memberId);
-            response.put("message", "MemberId가 존재합니다.");
-            return ResponseEntity.ok(response);
-        } catch (MemberException e) {
-            response.put("error", e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-        }
-    }
-
-    @Operation(summary = "회원 가입", description = "로컬 유저 정보를 DB에 저장한다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "로컬 유저 가입 성공")
-    })
-    @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> registerLocalMember(@RequestBody MemberRequest memberRequest) {
-
-        memberService.registerMember(memberRequest);
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "로컬 유저 가입 성공");
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-    @Operation(summary = "아이디 찾기", description = "로컬 유저 아이디를 반환한다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "아이디 찾기 성공")
-    })
-    @PostMapping("/find-id")
-    public ResponseEntity<Map<String, String>> findMemberIdByEmail(@RequestBody MemberSimpleRequest memberRequest) {
-
-        String email = memberRequest.email();
-        String memberId = memberService.findMemberIdByEmail(email);
-        Map<String, String> response = new HashMap<>();
-        response.put("memberId", memberId);
-        return ResponseEntity.ok(response);
-    }
-
-    @Operation(summary = "비밀번호 찾기", description = "로컬 유저 비밀번호를 재설정한다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "비밀번호 재설정 성공")
-    })
-    @PutMapping("/request-reset")
-    public ResponseEntity<Map<String, String>> updatePassword(@RequestBody MemberRequest memberRequest){
-        Map<String, String> response = new HashMap<>();
-        try {
-            memberService.updateMemberPassword(memberRequest.memberId(), memberRequest.password());
-            response.put("message", "비밀번호 재설정 성공");
-            return ResponseEntity.ok(response);
-        } catch (MemberException e) {
-            response.put("error", e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-        }
-    }
 
     @Operation(summary = "회원 정보 수정", description = "마이페이지용, 요청된 회원 정보로 DB를 수정한다.")
     @PutMapping("")
