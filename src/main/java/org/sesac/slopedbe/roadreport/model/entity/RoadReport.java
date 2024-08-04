784ec56a9d8d5a6ee64ceeff1e7b97450dbad64a
ChangHoSe <sas1397@naver.com>
2024-08-04 23:23:00 +0900

    Chore: road(#24) merge

--- src/main/java/org/sesac/slopedbe/roadreport/model/entity/RoadReport.java
+++ src/main/java/org/sesac/slopedbe/roadreport/model/entity/RoadReport.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.sesac.slopedbe.common.entity.BaseTimeEntity;
 import org.sesac.slopedbe.common.type.ReportStatus;
+import org.sesac.slopedbe.member.model.entity.Member;
 import org.sesac.slopedbe.road.model.entity.Road;
 
 import jakarta.persistence.Column;
@@ -14,6 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
