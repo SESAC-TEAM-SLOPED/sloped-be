cf4e6f050fbd34c9d7c48eb79fd5bf2a3c66c357
ChangHoSe <sas1397@naver.com>
2024-08-05 23:30:49 +0900

    Chore: merge 후 삭제 파일 반영 (#59)

--- src/main/java/org/sesac/slopedbe/member/model/DTO/request/CheckDuplicateIdRequest.java
+++
@@ -1,9 +0,0 @@
-package org.sesac.slopedbe.member.model.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record CheckDuplicateIdRequest (
-	@NotBlank(message = "아이디를 입력해주세요")
-	String id
-){
-}
