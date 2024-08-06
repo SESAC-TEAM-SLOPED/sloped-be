05be275f4ecf752fe9c085d6efb753aeb2415576
ChangHoSe <sas1397@naver.com>
2024-08-06 10:02:07 +0900

    Chore: 최신 dev merge, 주석 수정(#24)

--- src/main/java/org/sesac/slopedbe/auth/model/dto/response/NaverUserInfoResponse.java
+++ src/main/java/org/sesac/slopedbe/auth/model/dto/response/NaverUserInfoResponse.java
@@ -11,61 +11,37 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NaverUserInfoResponse {
 
-	// API 호출 결과 코드
 	@JsonProperty("resultcode")
-	private String resultCode;
-
-	// 호출 결과 메시지
+	private String resultCode; // API 호출 결과 코드
 	@JsonProperty("message")
-	private String message;
-
-	// 사용자 정보
+	private String message; // 호출 결과 메시지
 	@JsonProperty("response")
-	public Response response;
+	public Response response; // 사용자 정보
 
 	@Getter
 	@NoArgsConstructor
 	@JsonIgnoreProperties(ignoreUnknown = true)
 	public class Response {
 
-		// 동일인 식별 정보
 		@JsonProperty("id")
-		private String id;
-
-		// 사용자 별명
+		private String id; // 동일인 식별 정보
 		@JsonProperty("nickname")
-		private String nickname;
-
-		// 사용자 이름
+		private String nickname; // 사용자 별명
 		@JsonProperty("name")
-		private String name;
-
-		// 사용자 메일 주소
+		private String name; // 사용자 이름
 		@JsonProperty("email")
-		private String email;
-
-		// 성별 (F: 여성, M: 남성, U: 확인불가)
+		private String email; // 사용자 메일 주소
 		@JsonProperty("gender")
-		private String gender;
-
-		// 사용자 연령대
+		private String gender; // 성별 (F: 여성, M: 남성, U: 확인불가)
 		@JsonProperty("age")
-		private String age;
-
-		// 사용자 생일 (MM-DD 형식)
+		private String age; // 사용자 연령대
 		@JsonProperty("birthday")
-		private String birthday;
-
-		// 사용자 프로필 사진 URL
+		private String birthday; // 사용자 생일 (MM-DD 형식)
 		@JsonProperty("profile_image")
-		private String profileImage;
-
-		// 출생연도
+		private String profileImage; // 사용자 프로필 사진 URL
 		@JsonProperty("birthyear")
-		private String birthYear;
-
-		// 휴대전화번호
+		private String birthYear; // 출생연도
 		@JsonProperty("mobile")
-		private String mobile;
+		private String mobile; // 휴대전화번호
 	}
 }
