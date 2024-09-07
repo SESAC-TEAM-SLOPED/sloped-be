0b180612ae84532298453193d9b60e7d8196268d
bsjin1122 <kitty7579@hanmail.net>
2024-09-07 21:19:36 +0900

    Fix: 초기 데이터 csv 파일을 data.sql 문으로 추가함.

--- src/main/java/org/sesac/slopedbe/road/repository/RoadKoreaCityRepository.java
+++ src/main/java/org/sesac/slopedbe/road/repository/RoadKoreaCityRepository.java
@@ -9,16 +9,4 @@ public interface RoadKoreaCityRepository extends JpaRepository<RoadKoreaCity, Lo
 	@Query(value = "SELECT r.complaint_region FROM road_korea_city r WHERE r.city_name = :cityName LIMIT 1", nativeQuery = true)
 	String findComplaintRegionByCityName(@Param("cityName") String cityName);
 
-	@Query("SELECT r FROM RoadKoreaCity r WHERE r.regionName = :cityName")
-	RoadKoreaCity findByRegionName(@Param("cityName") String cityName);
-
-	@Query(value = "SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END " +
-		"FROM road_korea_city r " +
-		"WHERE r.city_name = :cityName " +
-		"AND r.region_name = :regionName " +
-		"AND r.complaint_region = :complaintRegion", nativeQuery = true)
-	boolean existsByCityNameAndRegionNameAndComplaintRegion(
-		@Param("cityName") String cityName,
-		@Param("regionName") String regionName,
-		@Param("complaintRegion") String complaintRegion);
 }
