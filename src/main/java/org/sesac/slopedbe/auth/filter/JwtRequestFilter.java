ed20af60a8d10cd4fc4daedda80ad3514d19432e
bsjin1122 <kitty7579@hanmail.net>
2024-09-07 22:00:04 +0900

    Merge branch 'chore/#SLOPE-76-setting-swagger' of C:\study\sloped-be with conflicts.

---
+++ src/main/java/org/sesac/slopedbe/auth/filter/JwtRequestFilter.java
@@ -0,0 +1,116 @@
+package org.sesac.slopedbe.auth.filter;
+
+import java.io.IOException;
+
+import org.sesac.slopedbe.auth.exception.JwtErrorCode;
+import org.sesac.slopedbe.auth.exception.JwtException;
+import org.sesac.slopedbe.auth.service.LoginServiceImpl;
+import org.sesac.slopedbe.auth.util.JwtUtil;
+import org.sesac.slopedbe.member.model.entity.MemberCompositeKey;
+import org.sesac.slopedbe.member.model.type.MemberOauthType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+	private LoginServiceImpl memberService;
+	private JwtUtil jwtUtil;
+
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		String path = request.getRequestURI();
+		boolean shouldNotFilter = path.startsWith("/api/auth/") || path.startsWith("/api/facilities") || path.startsWith("/api/roads") || path.startsWith("/favicon.ico") || path.startsWith("/login/oauth2")
+			|| path.startsWith("/api/roadReport/connect-center")
+			|| path.startsWith("/api/roadReport/connect-callTaxi")
+			|| path.startsWith("/api/roadReport/info/")
+			|| path.startsWith("/api/roadReport/get-centerList")
+			|| path.startsWith("/api/gpt")
+			// Swagger 경로 허용
+			|| path.startsWith("/swagger-ui/")
+			|| path.startsWith("/v3/api-docs")
+			|| path.startsWith("/swagger-resources/")
+			|| path.startsWith("/webjars/")
+			|| path.startsWith("/swagger-ui.html")
+			|| path.startsWith("/swagger-resources/configuration/ui")
+			|| path.startsWith("/swagger-resources/configuration/security");
+		log.info("Request path: {}, ShouldNotFilter: {}", path, shouldNotFilter);
+		return shouldNotFilter;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+		throws ServletException, IOException {
+
+		if (shouldNotFilter(request)) {
+			log.info("Skipping JWT filter for path: {}", request.getRequestURI());
+			chain.doFilter(request, response);
+			return;
+		}
+
+		final String authorizationHeader = request.getHeader("Authorization");
+		validateAuthorizationHeader(authorizationHeader);
+
+		String token = authorizationHeader.substring(7);
+		validateToken(token);
+
+		MemberCompositeKey compositeKey = extractCompositeKey(token);
+		authenticateUser(request, compositeKey);
+
+		chain.doFilter(request, response);
+	}
+
+	private void validateAuthorizationHeader(String authorizationHeader) {
+		if (authorizationHeader == null || authorizationHeader.isBlank()) {
+			throw new JwtException(JwtErrorCode.JWT_NOT_FOUND);
+		}
+
+		if (!authorizationHeader.startsWith("Bearer ")) {
+			throw new JwtException(JwtErrorCode.JWT_PREFIX_INVALID);
+		}
+	}
+
+	private void validateToken(String token) {
+		if (token == null || token.isBlank()) {
+			throw new JwtException(JwtErrorCode.JWT_NOT_FOUND);
+		}
+	}
+
+	private MemberCompositeKey extractCompositeKey(String token) {
+		try {
+			String email = jwtUtil.extractEmailFromToken(token);
+			MemberOauthType oauthType = jwtUtil.extractOAuthTypeFromToken(token);
+			return new MemberCompositeKey(email, oauthType);
+		} catch (SignatureException e) {
+			throw new JwtException(JwtErrorCode.JWT_INVALID);
+		} catch (ExpiredJwtException e) {
+			throw new JwtException(JwtErrorCode.JWT_EXPIRED);
+		}
+	}
+
+	private void authenticateUser(HttpServletRequest request, MemberCompositeKey compositeKey) {
+		UserDetails userDetails = this.memberService.loadUserByUsername(LoginServiceImpl.createCompositeKey(compositeKey.getEmail(), compositeKey.getOauthType()));
+		log.info("Loaded UserDetails: {}", userDetails.getUsername());
+		UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+		usernamePasswordAuthenticationToken
+			.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+		SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+		log.info("Authentication set in SecurityContextHolder: {}", usernamePasswordAuthenticationToken.getPrincipal());
+	}
+}
---
+++ src/main/java/org/sesac/slopedbe/config/SecurityConfig.java
@@ -0,0 +1,90 @@
+package org.sesac.slopedbe.config;
+
+import java.util.Arrays;
+
+import org.sesac.slopedbe.auth.filter.JwtRequestFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+@Slf4j
+public class SecurityConfig {
+
+	private final JwtRequestFilter jwtRequestFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)  // CSRF 보호 비활성화
+			.cors(Customizer.withDefaults())
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.authorizeHttpRequests(authorizeRequests ->
+				authorizeRequests
+					// 로그인 필요 없는 경로 && 로그인/비로그인 모두 가능한 경로
+					.requestMatchers("/api/auth/**", "/api/facilities/**", "/api/roads/**","/api/roadReport/connect-center","/api/gpt/**",
+						"/api/roadReport/info/{roadReportId}", "/api/roadReport/get-centerList", "/api/roadReport/connect-callTaxi",
+						"/api/gpt", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/login/oauth2/**",
+						// Swagger 경로 허용
+						"/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/swagger-ui.html", "/swagger-resources/configuration/ui",
+						"/swagger-resources/configuration/security"
+					).permitAll()
+					// 로그인 필요한 경로
+					.requestMatchers("/api/users/**","/api/reviews/**","/api/users", "/api/roadReport/upload", "api/users/bookmark/").authenticated()
+					.anyRequest().authenticated()
+			)
+			.sessionManagement(sessionManagement ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://togetheroad.me", "https://www.togetheroad.me"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		configuration.setAllowCredentials(true);
+		configuration.setMaxAge(3600L); // 1시간 동안 preflight 요청 결과를 캐시
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+
+}
+
